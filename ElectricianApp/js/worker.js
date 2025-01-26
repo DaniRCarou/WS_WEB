22d0d8350a7865e04fc2a084caf5425038a33561
Dani <danicarou.dev@gmail.com>
2025-01-26 18:01:59 +0100

    The functionality of the buttons has been created so that the work and schedule forms appear and disappear. The special button form has been added, with a scrollbar, and a display has been added to show the total hours worked in a single day.

---
+++ ElectricianApp/js/worker.js
@@ -0,0 +1,273 @@
+
+/*
+
+// Get the "task btn" button
+const taskButton = document.querySelectorAll('.task_btn'); // Selecting all elements with the class task_btn on the page.
+
+// Add a click event for each button. It is looped over each button in the taskButton NodeList and attach a click event listener to each one. When a button is clicked, the function inside the event listener is executed.
+taskButton.forEach(button => { 
+
+    button.addEventListener('click', () => {
+
+        // Select the corresponding task container. Used to find the closest ancestor element of the clicked button that has the class formular. This helps us get the container that contains the button and the elements we want to show/hide.
+        const container = button.closest('.formular');
+
+        // Show the elements inside that container. QuerySelector is used to select the first elements with the classes .fa, .hours, and .hours_btn inside the selected container.
+        const fa = container.querySelector('.fa');
+        const hours = container.querySelector('.hours');
+        const hoursBtn = container.querySelector('.hours_btn');
+
+        if (fa.style.display === 'flex' && hours.style.display === 'flex' && hoursBtn.style.display === 'flex') { // it is checked if the display style of the fa, hours, and hoursBtn elements is set to 'flex'. If all three are visible (i.e., display: 'flex'), the condition evaluates to true.
+
+            // If they are visible, hide them
+            fa.style.display = 'none';
+            hours.style.display = 'none';
+            hoursBtn.style.display = 'none';
+
+        } else {
+
+            // If they are not visible, show them
+            fa.style.display = 'flex';
+            hours.style.display = 'flex';
+            hoursBtn.style.display = 'flex';
+
+        }
+    });
+
+});
+
+*/
+
+
+
+
+
+// Get the input for the date field
+const dateInput = document.getElementById('date');
+
+// Get today's date in YYYY-MM-DD format
+const today = new Date();
+const yyyy = today.getFullYear();
+const mm = String(today.getMonth() + 1).padStart(2, '0'); // Meses: 0-11, se ajusta a 1-12
+const dd = String(today.getDate()).padStart(2, '0');
+
+const formattedDate = `${yyyy}-${mm}-${dd}`;
+
+// Set today's date as the initial value
+dateInput.value = formattedDate;
+
+
+
+
+
+
+
+
+
+// 1. Get the "task_btn" button and the elements inside the .formular container
+
+const taskButton = document.querySelector('.task_btn');
+const fa = document.querySelector('.fa');
+const hours = document.querySelector('.hours');
+const hoursBtn = document.querySelector('.hours_btn');
+
+// 2. Add a click event to the button
+
+taskButton.addEventListener('click', () => {
+
+    // 3. To checik if the elements are visible
+
+    if (fa.style.display === 'flex' && hours.style.display === 'flex' && hoursBtn.style.display === 'flex') {
+
+    // 4. If they are visible, hide them
+
+        fa.style.display = 'none';
+        hours.style.display = 'none';
+        hoursBtn.style.display = 'none';
+
+        meetingTime.style.display = 'none';
+        agreeButton.style.display = 'none';
+        cleaninUpTime.style.display = 'none';
+        cleanUpAgreeButton.style.display = 'none';
+        specialWrapper.style.display = 'none';
+
+    } else {
+
+    // 5. If they are not visible, show them
+
+        fa.style.display = 'flex';
+        hours.style.display = 'flex';
+        hoursBtn.style.display = 'flex';
+
+        meetingTime.style.display = 'none';
+        agreeButton.style.display = 'none';
+        cleaninUpTime.style.display = 'none';
+        cleanUpAgreeButton.style.display = 'none';
+        specialWrapper.style.display = 'none';
+
+    }
+
+});
+
+
+
+
+
+
+
+
+
+// 1. Get the "teammeeting_btn" button and the elements inside the .formular container
+
+const teamMeetingButton = document.querySelector('.teammeeting_btn');
+const meetingTime = document.querySelector('.meeting_time');
+const agreeButton = document.querySelector('.agree_btn');
+
+
+// 2. Add a click event to the button
+
+teamMeetingButton.addEventListener('click', () => {
+
+    // 3. To checik if the elements are visible
+
+    if (meetingTime.style.display === 'flex' && agreeButton.style.display  === 'flex') {
+
+    // 4. If they are visible, hide them
+
+        
+        meetingTime.style.display = 'none';
+        agreeButton.style.display = 'none';
+        
+        fa.style.display = 'none';
+        hours.style.display = 'none';
+        hoursBtn.style.display = 'none';
+        cleaninUpTime.style.display = 'none';
+        cleanUpAgreeButton.style.display = 'none';
+        specialWrapper.style.display = 'none';
+
+    } else {
+
+    // 5. If they are not visible, show them
+
+        meetingTime.style.display = 'flex';
+        agreeButton.style.display = 'flex';
+        
+        fa.style.display = 'none';
+        hours.style.display = 'none';
+        hoursBtn.style.display = 'none';
+        cleaninUpTime.style.display = 'none';
+        cleanUpAgreeButton.style.display = 'none';
+        specialWrapper.style.display = 'none';
+
+    }
+
+});
+
+
+
+
+
+
+
+
+
+// 1. Get the "cleanup_btn" button and the elements inside the .formular container
+
+const cleanUpButton = document.querySelector('.cleanup_btn');
+const cleaninUpTime = document.querySelector('.cleanup_time');
+const cleanUpAgreeButton = document.querySelector('.cleanup_agree_btn');
+
+
+// 2. Add a click event to the button
+
+cleanUpButton.addEventListener('click', () => {
+
+    // 3. To checik if the elements are visible
+
+    if (cleaninUpTime.style.display === 'flex' && cleanUpAgreeButton.style.display  === 'flex') {
+
+    // 4. If they are visible, hide them
+
+        cleaninUpTime.style.display = 'none';
+        cleanUpAgreeButton.style.display = 'none';
+        
+        meetingTime.style.display = 'none';
+        agreeButton.style.display = 'none';
+        fa.style.display = 'none';
+        hours.style.display = 'none';
+        hoursBtn.style.display = 'none';
+        specialWrapper.style.display = 'none';
+
+    } else {
+
+    // 5. If they are not visible, show them
+
+        cleaninUpTime.style.display = 'flex';
+        cleanUpAgreeButton.style.display = 'flex';
+        
+        meetingTime.style.display = 'none';
+        agreeButton.style.display = 'none';
+        fa.style.display = 'none';
+        hours.style.display = 'none';
+        hoursBtn.style.display = 'none';
+        specialWrapper.style.display = 'none';
+
+    }
+
+});
+
+
+
+
+
+
+
+
+
+// 1. Get the "cleanup_btn" button and the elements inside the .formular container
+
+const specialButton = document.querySelector('.special_btn');
+const specialWrapper = document.querySelector('.special_wrapper');
+const scrollWrapper = document.querySelector('.scroll_wrapper');
+
+// 2. Add a click event to the button
+
+specialButton.addEventListener('click', () => {
+
+    // 3. To checik if the elements are visible
+
+    if (specialWrapper.style.display === 'flex' && scrollWrapper.style.display === 'flex' ) {
+
+    // 4. If they are visible, hide them
+
+        specialWrapper.style.display = 'none';
+        scrollWrapper.style.display = 'none';
+
+        cleaninUpTime.style.display = 'none';
+        cleanUpAgreeButton.style.display = 'none';        
+        meetingTime.style.display = 'none';
+        agreeButton.style.display = 'none';
+        fa.style.display = 'none';
+        hours.style.display = 'none';
+        hoursBtn.style.display = 'none';
+
+
+    } else {
+
+    // 5. If they are not visible, show them
+
+        specialWrapper.style.display = 'flex';
+        scrollWrapper.style.display = 'flex';
+
+        cleaninUpTime.style.display = 'none';
+        cleanUpAgreeButton.style.display = 'none';        
+        meetingTime.style.display = 'none';
+        agreeButton.style.display = 'none';
+        fa.style.display = 'none';
+        hours.style.display = 'none';
+        hoursBtn.style.display = 'none';
+        
+
+    }
+
+});
